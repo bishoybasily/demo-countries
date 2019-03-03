f42a480b03e1dfe28a380ac687e733f6a0a8a9b6
bishoy <Z01141387925A@a>
2019-03-03 17:12:16 +0200

    repo setup

---
+++ src/main/java/com/gmail/bishoybasily/democountries/DemoCountriesApplication.java
@@ -0,0 +1,13 @@
+package com.gmail.bishoybasily.democountries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCountriesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoCountriesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gmail/bishoybasily/democountries/DemoCountriesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gmail.bishoybasily.democountries;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoCountriesApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
